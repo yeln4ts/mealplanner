41e542f031cf9cf1dcc740d2d028723d0b2a1b3f
yeln4ts <yeln4ts@users.noreply.github.com>
2026-01-29 21:59:36 -0500

    update vite config

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    allowedHosts: 'all'
+    allowedHosts: 'all',
     host: true,
     port: 5173
   }
